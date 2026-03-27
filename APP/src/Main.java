28505fa601ca44da40f027acf64b4e9573607860
Avinash-1464 <ponneboinaavinash@gmail.com>
2026-03-27 21:36:18 +0530

    Use Case 1: Application Entry & Welcome Message

--- APP/src/Main.java
+++ APP/src/Main.java
@@ -2,6 +2,5 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Book My Stay App");
         System.out.println("Application: Hotel Booking System");
-
     }
 }
